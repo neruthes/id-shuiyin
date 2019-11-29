c5774ef816bcf624cd3f6947dede78ac7b25bc85
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2019-11-29 08:03:08 +0000

    hello world

---
+++ README.md
@@ -0,0 +1,1 @@
+https://joyneop.xyz/id-shuiyin/
---
+++ js/main.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', function () {
+    var renderer = function () {
+        // Generate
+        (function () {
+            var myCanvas = document.querySelector("#my-canvas-area");
+            var data = {
+                dateStr: (function () {
+                    var lp2 = function (inputStr) {
+                        if (inputStr.length >= 2) {
+                            return inputStr;
+                        } else {
+                            return (new Array(2-inputStr.length)).fill(0) + inputStr;
+                        }
+                    };
+                    var d = (new Date(Date.now()+7*24*3600*1000));
+                    dateStr = d.toISOString().slice(0, 10).concat('日').replace(
+                        '-', '年'
+                    ).replace(
+                        '-', '月'
+                    ).replace(
+                        /\d/g,
+                        function (num) {
+                            return '零一二三四五六七八九'.split('')[num];
+                        }
+                    ).replace(
+                        /一([一二])月/,
+                        '十$1月'
+                    );
+                    console.log(dateStr);
+                    return dateStr;
+                })(),
+                jsInput_orgName: document.querySelector('#jsInput-orgName').value
+            };
+            myCanvas.innerHTML = (new Array(100)).fill(1).map(function (v, i) {
+                return `<span class="WM-span" style="color: #${
+                    ['F00','080','000'][i%3]
+                }">仅供【${data.jsInput_orgName}】使用，其他用途无效，${data.dateStr}后无效！！！</span>`
+            }).join('');
+        })();
+        // Export
+        html2canvas(document.querySelector("#my-canvas-area"), {
+            backgroundColor: null,
+            width: 600,
+            height: 600
+        }).then(canvas => {
+            var img = document.createElement('img');
+            img.src = canvas.toDataURL();
+            document.querySelector('#my-img-export').innerHTML = '';
+            document.querySelector('#my-img-export').appendChild(img);
+        });
+    };
+    renderer();
+    document.querySelector('#jsBtn-generate').addEventListener('click', renderer);
+});
